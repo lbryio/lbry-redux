fec46128671bfb6701b5674dc9b04888875a05b9
Sean Yesmunt <sean.yesmunt@gmail.com>
2018-05-15 23:26:31 -0400

    use CLAIM_LIST instead of CLAIM_LIST_MINE

--- dist/bundle.js
+++ dist/bundle.js
@@ -4587,11 +4587,11 @@ reducers[ACTIONS.FETCH_CLAIM_LIST_MINE_COMPLETED] = function (state, action) {
   });
 };
 
-reducers[ACTIONS.FETCH_CHANNEL_LIST_MINE_STARTED] = function (state) {
+reducers[ACTIONS.FETCH_CHANNEL_LIST_STARTED] = function (state) {
   return Object.assign({}, state, { fetchingMyChannels: true });
 };
 
-reducers[ACTIONS.FETCH_CHANNEL_LIST_MINE_COMPLETED] = function (state, action) {
+reducers[ACTIONS.FETCH_CHANNEL_LIST_COMPLETED] = function (state, action) {
   var claims = action.data.claims;
 
   var myChannelClaims = new Set(state.myChannelClaims);
