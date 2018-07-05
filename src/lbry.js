6a21ea7d72eca245c6b275d827b8cbeed6d5956c
Baltazar Gomez <btzr.io@gmail.com>
2018-07-05 12:20:41 -0600

    use file extension for media detection

--- src/lbry.js
+++ src/lbry.js
@@ -109,13 +109,13 @@ Lbry.connect = () => {
   return Lbry.connectPromise;
 };
 
-Lbry.getMediaType = (contentType, fileName) => {
-  if (fileName) {
+Lbry.getMediaType = (contentType, extname) => {
+  if (extname) {
     const formats = [
-      [/^.+\.(mp4|m4v|webm|flv|f4v|ogv)$/i, 'video'],
-      [/^.+\.(mp3|m4a|aac|wav|flac|ogg|opus)$/i, 'audio'],
-      [/^.+\.(html|htm|xml|pdf|odf|doc|docx|md|markdown|txt|epub|org)$/i, 'document'],
-      [/^.+\.(stl|obj|fbx|gcode)$/i, '3D-file'],
+      [/^(mp4|m4v|webm|flv|f4v|ogv)$/i, 'video'],
+      [/^(mp3|m4a|aac|wav|flac|ogg|opus)$/i, 'audio'],
+      [/^(html|htm|xml|pdf|odf|doc|docx|md|markdown|txt|epub|org)$/i, 'document'],
+      [/^(stl|obj|fbx|gcode)$/i, '3D-file'],
     ];
     const res = formats.reduce((ret, testpair) => {
       switch (testpair[0].test(ret)) {
@@ -124,8 +124,8 @@ Lbry.getMediaType = (contentType, fileName) => {
         default:
           return ret;
       }
-    }, fileName);
-    return res === fileName ? 'unknown' : res;
+    }, extname);
+    return res === extname ? 'unknown' : res;
   } else if (contentType) {
     return /^[^/]+/.exec(contentType)[0];
   } 
