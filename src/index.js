2ff9f70a3d765946a1c83c8e7eee7d81c96c1345
Sean Yesmunt <sean@lbry.io>
2019-01-28 14:38:28 -0500

    update: add new selector for first item in list of related content

--- src/index.js
+++ src/index.js
@@ -115,6 +115,7 @@ export {
   makeSelectNsfwCountFromUris,
   makeSelectNsfwCountForChannel,
   makeSelectRecommendedContentForUri,
+  makeSelectFirstRecommendedFileForUri,
   makeSelectChannelForClaimUri,
   makeSelectClaimIsPending,
   makeSelectPendingByUri,
