cca78e9341f5e3049776bdd6f2cb32907ada96f6
Sean Yesmunt <sean@lbry.io>
2020-06-29 17:02:19 -0400

    Revert "remove comment/blocked code"
    This reverts commit e4c05cebe97b278eca0f7e1c24fa0c210132abd2.

--- src/index.js
+++ src/index.js
@@ -135,6 +135,16 @@ export {
 
 export { doToggleTagFollow, doAddTag, doDeleteTag } from 'redux/actions/tags';
 
+export {
+  doCommentList,
+  doCommentCreate,
+  doCommentAbandon,
+  doCommentHide,
+  doCommentUpdate,
+} from 'redux/actions/comments';
+
+export { doToggleBlockChannel } from 'redux/actions/blocked';
+
 export { doPopulateSharedUserState, doPreferenceGet, doPreferenceSet } from 'redux/actions/sync';
 
 // utils
@@ -145,12 +155,14 @@ export { isClaimNsfw, createNormalizedClaimSearchKey } from 'util/claim';
 
 // reducers
 export { claimsReducer } from 'redux/reducers/claims';
+export { commentReducer } from 'redux/reducers/comments';
 export { contentReducer } from 'redux/reducers/content';
 export { fileInfoReducer } from 'redux/reducers/file_info';
 export { notificationsReducer } from 'redux/reducers/notifications';
 export { publishReducer } from 'redux/reducers/publish';
 export { searchReducer } from 'redux/reducers/search';
 export { tagsReducer } from 'redux/reducers/tags';
+export { blockedReducer } from 'redux/reducers/blocked';
 export { walletReducer } from 'redux/reducers/wallet';
 
 // selectors
@@ -239,6 +251,8 @@ export {
   selectMyPurchasesCount,
   selectPurchaseUriSuccess,
 } from 'redux/selectors/claims';
+
+export { makeSelectCommentsForUri, selectIsFetchingComments } from 'redux/selectors/comments';
 
 export {
   makeSelectFileInfoForUri,
@@ -347,3 +361,9 @@ export {
   selectUnfollowedTags,
   makeSelectIsFollowingTag,
 } from 'redux/selectors/tags';
+
+export {
+  selectBlockedChannels,
+  selectChannelIsBlocked,
+  selectBlockedChannelsCount,
+} from 'redux/selectors/blocked';
