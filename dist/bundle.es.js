90012bf47c170f244039261548dab7c7597046dc
jessop <jessop@lbry.com>
2020-09-16 09:56:24 -0400

    updates supporting sync fixes
    change alt preference key to local
    
    remove enable_sync from sync list
    
    restore to enable_sync in client settings

--- dist/bundle.es.js
+++ dist/bundle.es.js
@@ -957,7 +957,7 @@ var daemon_settings = /*#__PURE__*/Object.freeze({
 const SDK_SYNC_KEYS = [LBRYUM_SERVERS, SHARE_USAGE_DATA];
 
 // CLIENT
-const CLIENT_SYNC_KEYS = [SHOW_MATURE, HIDE_REPOSTS, SHOW_ANONYMOUS, INSTANT_PURCHASE_ENABLED, INSTANT_PURCHASE_MAX, THEME, AUTOPLAY, HIDE_BALANCE, HIDE_SPLASH_ANIMATION, FLOATING_PLAYER, DARK_MODE_TIMES, AUTOMATIC_DARK_MODE_ENABLED, ENABLE_SYNC];
+const CLIENT_SYNC_KEYS = [SHOW_MATURE, HIDE_REPOSTS, SHOW_ANONYMOUS, INSTANT_PURCHASE_ENABLED, INSTANT_PURCHASE_MAX, THEME, AUTOPLAY, HIDE_BALANCE, HIDE_SPLASH_ANIMATION, FLOATING_PLAYER, DARK_MODE_TIMES, AUTOMATIC_DARK_MODE_ENABLED];
 
 var shared_preferences = /*#__PURE__*/Object.freeze({
   SDK_SYNC_KEYS: SDK_SYNC_KEYS,
@@ -1801,7 +1801,10 @@ function doPreferenceGet(key, success, fail) {
 const SHARED_PREFERENCE_VERSION = '0.1';
 let oldShared = {};
 
-const buildSharedStateMiddleware = (actions, sharedStateFilters, sharedStateCb) => ({ getState, dispatch }) => next => action => {
+const buildSharedStateMiddleware = (actions, sharedStateFilters, sharedStateCb) => ({
+  getState,
+  dispatch
+}) => next => action => {
   const currentState = getState();
 
   // We don't care if sync is disabled here, we always want to backup preferences to the wallet
@@ -1812,7 +1815,9 @@ const buildSharedStateMiddleware = (actions, sharedStateFilters, sharedStateCb) 
   const actionResult = next(action);
   // Call `getState` after calling `next` to ensure the state has updated in response to the action
   const nextState = getState();
-  const preferenceKey = nextState.user && nextState.user.user && nextState.user.user.has_verified_email ? 'shared' : 'anon';
+  const syncEnabled = nextState.settings && nextState.settings.clientSettings && nextState.settings.clientSettings.enable_sync;
+  const hasVerifiedEmail = nextState.user && nextState.user.user && nextState.user.user.has_verified_email;
+  const preferenceKey = syncEnabled && hasVerifiedEmail ? 'shared' : 'local';
   const shared = {};
 
   Object.keys(sharedStateFilters).forEach(key => {
@@ -2782,7 +2787,7 @@ function doSendDraftTransaction(address, amount) {
           type: SEND_TRANSACTION_COMPLETED
         });
         dispatch(doToast({
-          message: __(`You sent ${amount} LBRY Credits`),
+          message: __('You sent %amount% LBRY Credits', { amount: amount }),
           linkText: __('History'),
           linkTarget: '/wallet'
         }));
