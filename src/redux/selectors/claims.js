feeb05f32ff257a10113b2ef69593a5df35da729
zeppi <jessopb@gmail.com>
2020-12-16 22:35:12 -0500

    support repost amount

--- src/redux/selectors/claims.js
+++ src/redux/selectors/claims.js
@@ -127,6 +127,7 @@ export const makeSelectClaimForUri = (uri: string, returnRepost: boolean = true)
             ...repostedClaim,
             repost_url: uri,
             repost_channel_url: channelUrl,
+            repost_amount: claim && claim.meta && claim.meta.effective_amount,
           };
         } else {
           return claim;
@@ -258,8 +259,8 @@ export const makeSelectMyPurchasesForPage = (query: ?string, page: number = 1) =
       const end = Number(page) * Number(PAGE_SIZE);
       return matchingFileInfos && matchingFileInfos.length
         ? matchingFileInfos
-            .slice(start, end)
-            .map(fileInfo => fileInfo.canonical_url || fileInfo.permanent_url)
+          .slice(start, end)
+          .map(fileInfo => fileInfo.canonical_url || fileInfo.permanent_url)
         : [];
     }
   );
@@ -365,8 +366,8 @@ export const makeSelectDateForUri = (uri: string) =>
         (claim.value.release_time
           ? claim.value.release_time * 1000
           : claim.meta && claim.meta.creation_timestamp
-          ? claim.meta.creation_timestamp * 1000
-          : null);
+            ? claim.meta.creation_timestamp * 1000
+            : null);
       if (!timestamp) {
         return undefined;
       }
