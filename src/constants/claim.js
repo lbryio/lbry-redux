bf728f8716385749370de9a1edee50b267d61fa4
Sean Yesmunt <sean@lbry.io>
2021-03-11 12:56:34 -0500

    update staked amount constants

--- src/constants/claim.js
+++ src/constants/claim.js
@@ -4,8 +4,8 @@ export const CHANNEL_ANONYMOUS = 'anonymous';
 export const CHANNEL_NEW = 'new';
 export const PAGE_SIZE = 20;
 
-export const LEVEL_1_STAKED_AMOUNT = 1;
-export const LEVEL_2_STAKED_AMOUNT = 2;
-export const LEVEL_3_STAKED_AMOUNT = 3;
-export const LEVEL_4_STAKED_AMOUNT = 4;
-export const LEVEL_5_STAKED_AMOUNT = 5;
+export const LEVEL_1_STAKED_AMOUNT = 0;
+export const LEVEL_2_STAKED_AMOUNT = 1;
+export const LEVEL_3_STAKED_AMOUNT = 50;
+export const LEVEL_4_STAKED_AMOUNT = 250;
+export const LEVEL_5_STAKED_AMOUNT = 1000;
