990cbcab4daa0d93817aeb0c73b53848a068de73
zeppi <jessopb@gmail.com>
2021-03-25 18:49:48 -0400

    provide selector for placeholder stream

--- src/index.js
+++ src/index.js
@@ -189,6 +189,7 @@ export {
   makeSelectAbandoningClaimById,
   makeSelectIsAbandoningClaimForUri,
   makeSelectClaimHasSource,
+  makeSelectClaimIsStreamPlaceholder,
   selectPendingIds,
   selectReflectingById,
   makeSelectClaimForClaimId,
