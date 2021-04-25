66c77fc39b9af7796b7461921c534141c856bab2
zeppi <jessopb@gmail.com>
2021-04-24 21:23:34 -0400

    delay preference set two seconds

--- dist/bundle.es.js
+++ dist/bundle.es.js
@@ -1850,9 +1850,10 @@ function doPreferenceGet(key, success, fail) {
 
 //      
 
+const RUN_PREFERENCES_DELAY_MS = 2000;
 const SHARED_PREFERENCE_VERSION = '0.1';
 let oldShared = {};
-
+let timeout;
 const buildSharedStateMiddleware = (actions, sharedStateFilters, sharedStateCb) => ({
   getState,
   dispatch
@@ -1863,38 +1864,42 @@ const buildSharedStateMiddleware = (actions, sharedStateFilters, sharedStateCb) 
   if (!actions.includes(action.type) || typeof action === 'function') {
     return next(action);
   }
-
+  clearTimeout(timeout);
   const actionResult = next(action);
   // Call `getState` after calling `next` to ensure the state has updated in response to the action
-  const nextState = getState();
-  const syncEnabled = nextState.settings && nextState.settings.clientSettings && nextState.settings.clientSettings.enable_sync;
-  const hasVerifiedEmail = nextState.user && nextState.user.user && nextState.user.user.has_verified_email;
-  const preferenceKey = syncEnabled && hasVerifiedEmail ? 'shared' : 'local';
-  const shared = {};
 
-  Object.keys(sharedStateFilters).forEach(key => {
-    const filter = sharedStateFilters[key];
-    const { source, property, transform } = filter;
-    let value = nextState[source][property];
-    if (transform) {
-      value = transform(value);
+  function runPreferences() {
+    const nextState = getState();
+    const syncEnabled = nextState.settings && nextState.settings.clientSettings && nextState.settings.clientSettings.enable_sync;
+    const hasVerifiedEmail = nextState.user && nextState.user.user && nextState.user.user.has_verified_email;
+    const preferenceKey = syncEnabled && hasVerifiedEmail ? 'shared' : 'local';
+    const shared = {};
+
+    Object.keys(sharedStateFilters).forEach(key => {
+      const filter = sharedStateFilters[key];
+      const { source, property, transform } = filter;
+      let value = nextState[source][property];
+      if (transform) {
+        value = transform(value);
+      }
+
+      shared[key] = value;
+    });
+
+    if (!isEqual(oldShared, shared)) {
+      // only update if the preference changed from last call in the same session
+      oldShared = shared;
+      dispatch(doPreferenceSet(preferenceKey, shared, SHARED_PREFERENCE_VERSION));
     }
 
-    shared[key] = value;
-  });
-
-  if (!isEqual(oldShared, shared)) {
-    // only update if the preference changed from last call in the same session
-    oldShared = shared;
-    dispatch(doPreferenceSet(preferenceKey, shared, SHARED_PREFERENCE_VERSION));
+    if (sharedStateCb) {
+      // Pass dispatch to the callback to consumers can dispatch actions in response to preference set
+      sharedStateCb({ dispatch, getState });
+    }
+    clearTimeout(timeout);
+    return actionResult;
   }
-
-  if (sharedStateCb) {
-    // Pass dispatch to the callback to consumers can dispatch actions in response to preference set
-    sharedStateCb({ dispatch, getState });
-  }
-
-  return actionResult;
+  timeout = setTimeout(runPreferences, RUN_PREFERENCES_DELAY_MS);
 };
 
 //      
